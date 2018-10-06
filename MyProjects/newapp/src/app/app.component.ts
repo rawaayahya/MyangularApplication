96bb3cad08ab6b24b4adbfea2efbd255e44b394b
rawaayahya <raoua.yahya2014@gmail.com>
2018-10-06 12:24:14 +0200

    how can you add inline html and css code on the component file.ts -> it is not recomanded to use it when we have a code with many lines

--- MyProjects/newapp/src/app/app.component.ts
+++ MyProjects/newapp/src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Bonjour !</h1>`,
+  styles: [`h1{
+    color: red;
+  }`]
 })
 export class AppComponent {
   nom:string;
