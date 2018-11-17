eb269c90bb6d3f3ec003d8593e834031c7879756
rawaayahya <raoua.yahya2014@gmail.com>
2018-11-17 22:36:55 +0100

    la directive *ngIf

--- MyProjects/newapp/src/app/app.component.ts
+++ MyProjects/newapp/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   titre:string='mon titre';
   nombre:number=10;
   bool:boolean=true;
+  display:boolean=false;
   //property binding nous permet une souplesse dans la definition des elements html
   // par exemple si vous voulez désactiver le click sur un boutton jusqu'à le remplissage 
   // de tous les champs dans le formulaire ;on peut lier la property disable avec un attribut dans le component
